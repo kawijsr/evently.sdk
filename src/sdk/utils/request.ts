40ae5e2d33016fd467a6aec6596621a4e2b48c45
wilfredojsr <wilfredojsr2@gmail.com>
2026-04-15 15:10:39 -0300

    fix: apiKey when EVENTLY_PRIVATE_API_KEY env is setted

--- src/sdk/utils/request.ts
+++ src/sdk/utils/request.ts
@@ -5,7 +5,7 @@ export const request = {
   baseUrl: CONSTANTS.API_URL,
   headers: () => ({
     'content-type': 'application/json',
-    'x-api-key': 'ApiKey ' + Evently.getInstance()?.apiKey || process.env.EVENTLY_PRIVATE_API_KEY,
+    'x-api-key': `ApiKey ${(Evently.getInstance()?.apiKey || process.env.EVENTLY_PRIVATE_API_KEY)}`,
   }),
   buildResponse: (response) => {
     if (!response.ok) {
